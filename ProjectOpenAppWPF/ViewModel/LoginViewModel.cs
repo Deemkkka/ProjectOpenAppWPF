c6a8d28307ce656661c2810313f0cd8da2ec4758
Deem <Deem@DEEM>
2022-09-13 10:28:38 +0300

    Добавлены  базовые классы ViewModelBase и RelayCommand, также добавлен LoginViewModel

---
+++ ProjectOpenAppWPF/ViewModel/LoginViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProjectOpenAppWPF.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        //Fields
+        private string _username;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+        // Properties
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+        public SecureString Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage
+        {
+            get => _errorMessage; set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible
+        {
+            get => _isViewVisible; set
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+        // Commands
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RecoverPasswordCommand = new RelayCommand(p => ExecuteRecoverPasswordCommand("", "");
+        }
+
+        private void ExecuteRecoverPasswordCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrWhiteSpace(Username) || Username.Length<3 ||
+                Password == null || Password.Length < 3)
+            {
+                validData = false;
+            }
+            else
+                validData = true;
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
